b70d64213e3e3764c3982e7174cd26abb61c7a56
jonasbj99 <99955044+jonasbj99@users.noreply.github.com>
2023-12-11 03:01:55 +0100

    BreathTracker Cleanup

--- Assets/Scripts/BreathTracker.cs
+++ Assets/Scripts/BreathTracker.cs
@@ -9,11 +9,6 @@ public class BreathTracker : MonoBehaviour
     int breathPressure;
     int limit = 565;
 
-    string message;
-
-    //string inString = "Inhaling";
-    //string exString = "Exhaling";
-
     float breathRate = 3f;
     
     bool inhaling;
@@ -25,8 +20,6 @@ public class BreathTracker : MonoBehaviour
     float exhaleCountdown;
 
     [SerializeField] TMP_Text inExText;
-    //[SerializeField] TMP_Text countInhaleText;
-    //[SerializeField] TMP_Text countExhaleText;
 
     [SerializeField] GameObject inhaleParent;
     [SerializeField] GameObject exhaleParent;
@@ -36,6 +29,7 @@ public class BreathTracker : MonoBehaviour
 
     void Update()
     {
+<<<<<<< Updated upstream
         /*
         if (Input.GetKey(KeyCode.Space))
         {
@@ -47,6 +41,8 @@ public class BreathTracker : MonoBehaviour
         }
         */
         
+=======
+>>>>>>> Stashed changes
         if (breathPressure < limit)
         {
             inhaling = true;
@@ -64,17 +60,6 @@ public class BreathTracker : MonoBehaviour
             inhaleCounter += Time.deltaTime;
             inhaleCountdown -= Time.deltaTime;
 
-            /* Countdown timer 
-            if (inhaleCountdown <= 0)
-            {
-                countInhaleText.text = "0.00";
-            }
-            else
-            {
-                countInhaleText.text = inhaleCountdown.ToString("0.00");
-            }
-            */
-
             CountdownLights(inhaleCountdown, inhaleLights);
         }
 
@@ -86,33 +71,18 @@ public class BreathTracker : MonoBehaviour
             exhaleCounter += Time.deltaTime;
             exhaleCountdown -= Time.deltaTime;
 
-            /* Countdown timer
-            if (exhaleCountdown <= 0)
-            {
-                countExhaleText.text = "0.00";
-            }
-            else
-            {
-                countExhaleText.text = exhaleCountdown.ToString("0.00");
-            }
-            */
-
             CountdownLights(exhaleCountdown, exhaleLights);
         }
 
         if (inhaleCounter > breathRate)
         {
             inExText.text = "Exhale";
-            //countInhaleText.gameObject.SetActive(false); Countdown timer
-            //countExhaleText.gameObject.SetActive(true); Countdown timer
             inhaleParent.SetActive(false);
             exhaleParent.SetActive(true);
         }
         else if (exhaleCounter > breathRate)
         {
             inExText.text = "Inhale";
-            //countExhaleText.gameObject.SetActive(false); Countdown timer
-            //countInhaleText.gameObject.SetActive(true); Countdown timer
             exhaleParent.SetActive(false);
             inhaleParent.SetActive(true);
         }
@@ -157,15 +127,11 @@ public class BreathTracker : MonoBehaviour
         }
     }
 
-    // Invoked when a line of data is received from the serial device.
     void OnMessageArrived(string msg)
     {
-        //Debug.Log("Arrived: " + msg);
         int.TryParse(msg, out breathPressure);
     }
-    // Invoked when a connect/disconnect event occurs. The parameter 'success'
-    // will be 'true' upon connection, and 'false' upon disconnection or
-    // failure to connect.
+
     void OnConnectionEvent(bool success)
     {
         Debug.Log(success ? "Device connected" : "Device disconnected");
